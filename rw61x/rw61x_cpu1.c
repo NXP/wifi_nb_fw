e0d68cb7f9cf7e98ce91619042533e47b9e2d3af
rex.chen <rex.chen_1@nxp.com>
2025-07-22 14:16:24 +0800

    Enhance redfinch uart wifi bridge download FW
    [Description]
    None monolithic download fw failed, so enhance redfinch uart wifi bridge download FW proccess
    
    [Root Cause]
    None monolithic download fw failed
    
    [Fix]
    None monolithic download fw from IMU default addr
    
    [Testing]
    Test pass on both RD and FRDM boards
    
    Change-Id: 7acd4722862211efbde95b8c49729026
    
    Signed-off-by: rex.chen <rex.chen_1@nxp.com>

--- rw61x/rw61x_cpu1.c
+++ rw61x/rw61x_cpu1.c
@@ -13,11 +13,7 @@
 #if CONFIG_MONOLITHIC_WIFI
 const uint8_t fw_cpu1[] __attribute__ ((aligned(4))) = {
 #if CONFIG_SOC_SERIES_RW6XX_REVISION_A2
-#if CONFIG_MFG_MONOLITHIC
-    #include <rw61xw_sb_mfg_fw_cpu1_a2.bin.inc>
-#else
     #include <rw61x_sb_wifi_a2.bin.inc>
-#endif
 #else
 #error "Couldn't determine soc revision, please define CONFIG_SOC_SERIES_RW6XX_REVISION_A2"
 #endif
--- rw61x/rw61x_cpu2.c
+++ rw61x/rw61x_cpu2.c
@@ -13,11 +13,7 @@
 #if CONFIG_MONOLITHIC_BLE_15_4
 const uint8_t fw_cpu2_combo[] __attribute__ ((aligned(4))) = {
 #if CONFIG_SOC_SERIES_RW6XX_REVISION_A2
-#if CONFIG_MFG_MONOLITHIC
-    #include <rw61xn_sb_mfg_fw_cpu2_combo_a2.bin.inc>
-#else
     #include <rw61x_sb_ble_15d4_combo_a2.bin.inc>
-#endif
 #else
 #error "Couldn't determine soc revision, please define CONFIG_SOC_SERIES_RW6XX_REVISION_A2"
 #endif
@@ -27,11 +23,7 @@ const uint8_t fw_cpu2_combo[] __attribute__ ((aligned(4))) = {
 
 const uint8_t fw_cpu2_ble[] __attribute__ ((aligned(4))) = {
 #if CONFIG_SOC_SERIES_RW6XX_REVISION_A2
-#if CONFIG_MFG_MONOLITHIC
-    #include <rw61xn_sb_mfg_fw_cpu2_ble_a2.bin.inc>
-#else
     #include <rw61x_sb_ble_a2.bin.inc>
-#endif
 #else
 #error "Couldn't determine soc revision, please define CONFIG_SOC_SERIES_RW6XX_REVISION_A2"
 #endif
